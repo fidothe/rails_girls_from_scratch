ce23f8716526eed2b31fadfc97a27de3e4f2d771
Matt Patterson <matt@reprocessed.org>
2013-03-23 16:56:36 +0100

    Fix up attendee migration

--- db/migrate/20130323150900_create_attendee.rb
+++ db/migrate/20130323150900_create_attendee.rb
@@ -1,13 +1,9 @@
 class CreateAttendee < ActiveRecord::Migration
-  def up
+  def change
   	create_table :attendees do |t|
   		t.string :name
 
       t.timestamps
   	end
-  end
-
-  def down
-    drop_table :attendees
   end
 end
